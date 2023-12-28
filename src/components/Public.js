531b296aa0b6c2a3c502e764fc4ddca380adbda1
GOKULRAJ <gokulrajlakshmananit@gmail.com>
2023-12-28 20:23:29 +0530

    Ready to Deploy

---
+++ src/components/Public.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+
+const Public = () => {
+    const content = (
+        <section className="public">
+            <header>
+                <h1>Welcome to <span className="nowrap">ByteNotes</span></h1>
+            </header>
+            <main className="public__main">
+                <p>Located in Chennai ,ByteNotes provides a trained staff ready to meet your tech repair needs.</p>
+                <address className="public__addr">
+                    GokulRaj L<br />
+                    Kattupalli<br />
+                    City-Chennai<br />
+                    <a href="tel:+9">9159929231</a>
+                </address>
+                <br />
+                <p>Owner:GokulRaj</p>
+            </main>
+            <footer>
+                <Link to="/login">Employee Login</Link>
+            </footer>
+        </section>
+
+    )
+    return content
+}
+export default Public
